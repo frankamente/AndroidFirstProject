57a0e84a834c69c406424eb81aed8e5cc5eeb52f
frankamente <fran29400@gmail.com>
2017-12-18 20:47:33 +0100

    Create Button and added click event. When Click occurs change the TextView

--- app/src/main/java/com/example/frankamente/myapplication/MainActivity.java
+++ app/src/main/java/com/example/frankamente/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.frankamente.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText("He cambiado");
+            }
+        });
     }
 }
